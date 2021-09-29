fbf011edbef120c7b7abed1c88c6b89ebab6fda1
shivanix <shvni.here@gmail.com>
2021-09-29 11:33:34 -0400

    used app.use, express.static to customize server to serve up the required folder

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
+const path = require('path')
+
 const express = require('express')
 
-const app = express()
+// console.log(path.join(__dirname, '../public'));
 
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) =>{
-  res.send('Hello express!')
+  res.send('<h1>Weather</h1>')
 
 })
 
-
-app.get('/help', (req, res)=>{
-  res.send('Help page')
-})
-
-app.get('/about', (req, res) =>{
-  res.send('About page')
-})
-
-app.get('/weather', (req, res)=>{
-res.send('Weather page')
-})
 
 app.listen(3000, ()=>{
   console.log('Server is up on Port 3000.');
