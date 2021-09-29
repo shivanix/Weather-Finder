f76d95097829be4eaf07872e153e021a242f29f8
shivanix <shvni.here@gmail.com>
2021-09-29 15:52:02 -0400

    set up express.static view engine to render templates

--- src/app.js
+++ src/app.js
@@ -4,13 +4,28 @@ const express = require('express')
 
 // console.log(path.join(__dirname, '../public'));
 
+
+/********************************************************** */
+// view engine tells Express which extension to associate with the template when you call res.render(). If you write:
+
+// app.set('view engine', 'hbs')
+// And then call:
+
+// res.render('home')
+// It'll look for a "home.hbs" file in your views directory so you don't need to specify the extension.
+
+/********************************************************** */
+// the express.static() middleware is what serves the CSS files. Whenever a request comes in, Express checks if the thing we're looking for is in the public folder and if it is, it serves it.
+
+/********************************************************** */
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) =>{
-  res.send('<h1>Weather</h1>')
+  res.render('index')
 
 })
 
