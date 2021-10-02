2ec5beee85a1039ed7c54c4ad6d9a30c669a82d2
Shivani <shvni.here@gmail.com>
2021-10-02 15:59:43 -0400

    Fixed the problem of extra curly braces around the require files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const path = require("path");
 const express = require("express");
 
-const hbs = require('hbs');
-const { response } = require("express");
+const hbs = require("hbs");
+const forecast = require("./utils/forecast");
+const geocode  = require("./utils/geocode");
 
 // console.log(path.join(__dirname, '../public'));
 
@@ -23,7 +24,7 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname,"../templates/partials")
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Views folder is a default location that Express expects your views to live in.
 
@@ -33,11 +34,10 @@ const partialsPath = path.join(__dirname,"../templates/partials")
 // So let's go ahead and define one similar to how we defined public directory path.
  */
 
-
 // Setup handlebars engine and viewslocation
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-hbs.registerPartials(partialsPath)
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -60,49 +60,64 @@ app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help",
     message: "Help page",
-    name: "Shivani"
+    name: "Shivani",
   });
 });
 
-app.get("/weather", (req,res) =>{
-
-  if(!req.query.address){
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
     return res.send({
-      error: "You must provide a valid address"
-    })
+      error: "You must provide a valid address",
+    });
   }
-  res.send({
-    forecast: 'Its is snowing',
-    location: 'Canada',
-    address: req.query.address 
-  })
-})
+  geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({
+        error
+      })
+    }
 
-app.get("/products", (req,res)=>{
-  if(!req.query.search) {
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({
+          error
+        })
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term'
-    })
+      error: "You must provide a search term",
+    });
   }
   console.log(req.query.search);
   res.send({
-    products: {}
-  })
-})
+    products: {},
+  });
+});
 
-app.get("/help/*", (req, res) =>{
+app.get("/help/*", (req, res) => {
   res.render("404", {
     title: 404,
-    message:"Help article not found"
-  })
-})
+    message: "Help article not found",
+  });
+});
 
-app.get('*', (req,res) =>{
+app.get("*", (req, res) => {
   res.render("404", {
     title: 404,
-    message:"Page not found"
-  })
-})
+    message: "Page not found",
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is up on Port 3000.");
