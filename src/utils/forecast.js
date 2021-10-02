3cd0ea2729bb6d634a61bc87aff1196b43a49c23
Shivani <shvni.here@gmail.com>
2021-10-02 14:37:39 -0400

    Using weatherStackApi for fetching weather info based on coords

---
+++ src/utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require("postman-request");
+const { weatherStackApiKey } = require("../APIKeys");
+
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=${weatherStackApiKey}&query=${encodeURIComponent(
+    longitude
+  )},${encodeURIComponent(latitude)}&units=f`;
+
+  // json in lowercase option, can be set to true or false.
+  // true responses are to automatically pass JSON responses to us.--You wont have to do JSON.parse specially
+  request({url, json: true }, (error, {body}) => {
+    const data = body.current;
+
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Invalid coordinate pair", undefined);
+    } else {
+      // console.log(data);
+
+      callback(
+        undefined,
+        `It is currently ${data.temperature} degrees Fahrenheit and is ${data.weather_descriptions[0]}. It feels like ${data.feelslike} degrees Fahrenheit outside`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
