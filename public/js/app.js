0db0e114dc5b76c3314d08f9ead0f6ac9b1b9ff4
Shivani <shvni.here@gmail.com>
2021-10-08 13:29:15 -0400

    Used fetch to get data from url; added event listener to form on submit

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
 console.log("Client side Js file is loaded");
+
+
+
+/* fetch allows you to fetch data from a URL and do something with it*/
+fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  });
+});
+
+fetch("http://localhost:3000/weather?address=toronto").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
+
+const weatherForm = document.querySelector('form')
+weatherForm.addEventListener('submit',(e)=>{
+  e.preventDefault()
+console.log("testingggggg");
+})
