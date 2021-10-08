f5196ddc4ac1e7c0c1d0fc04cb5ecf2fc27c9c5d
Shivani <shvni.here@gmail.com>
2021-10-08 14:28:15 -0400

    Added query selectors for the <p> tags; replaced console logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,30 @@
 console.log("Client side Js file is loaded");
 
-
-
 /* fetch allows you to fetch data from a URL and do something with it*/
-fetch("http://puzzle.mead.io/puzzle").then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
 
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
 
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-weatherForm.addEventListener('submit',(e)=>{
-  e.preventDefault()
+  const location = search.value;
+  messageOne.textContent = "Data loading...";
+  messageTwo.textContent = "";
 
-  const location = search.value
-
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = `${data.error}`;
+        } else {
+          messageOne.textContent = `${data.location}`;
+          messageTwo.textContent = `${data.forecast}`;
+        }
+      });
     }
-  });
+  );
 });
-console.log(location);
-})
